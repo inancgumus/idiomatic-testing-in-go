72a56eaa1d7fba2c34d00e99bce568097075228f
İnanç Gümüş <m@inanc.io>
2024-10-27 14:35:34 -0400

    Listing 7.9 Implementing the producer stage

---
+++ hit/pipe.go
@@ -0,0 +1,18 @@
+package hit
+
+import "net/http"
+
+// requestFunc is a type of function that creates a request.
+type requestFunc func() *http.Request
+
+// produce produces n requests and sends them to out.
+func produce(n int, genReq requestFunc) <-chan *http.Request {
+	out := make(chan *http.Request)
+	go func() {
+		defer close(out)
+		for range n {
+			out <- genReq()
+		}
+	}()
+	return out
+}
