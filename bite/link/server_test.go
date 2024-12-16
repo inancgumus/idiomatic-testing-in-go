ab3515bfdff2e130c0a8eb4502b6984f76218262
İnanç Gümüş <m@inanc.io>
2024-12-16 12:10:40 -0500

    Listing 9.18 Testing with a test helper

--- bite/link/server_test.go
+++ bite/link/server_test.go
@@ -1,6 +1,7 @@
 package link
 
 import (
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -12,7 +13,7 @@ func TestServerHealth(t *testing.T) {
 
 	var srv Server
 	w := httptest.NewRecorder()
-	srv.Health(w, httptest.NewRequest(http.MethodGet, "/", nil))
+	srv.Health(w, newRequest(t, http.MethodGet, "/", nil))
 
 	if w.Code != http.StatusOK {
 		t.Errorf("got status code = %d, want %d", w.Code, http.StatusOK)
@@ -20,4 +21,17 @@ func TestServerHealth(t *testing.T) {
 	if got := w.Body.String(); !strings.Contains(got, "OK") {
 		t.Errorf("got body = %s\twant contains %s", got, "OK")
 	}
+}
+
+// newRequest creates a new [http.Request] for testing.
+// It fails the calling test if it cannot create the request.
+func newRequest(
+	tb testing.TB, method string, target string, body io.Reader,
+) *http.Request {
+	tb.Helper()
+	r, err := http.NewRequest(method, target, body)
+	if err != nil {
+		tb.Fatalf("http.NewRequest() err = %v, want nil", err)
+	}
+	return r
 }
