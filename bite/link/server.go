67b6d06d143571e8e522e9c2d317e9cba397a0ba
İnanç Gümüş <m@inanc.io>
2024-12-18 02:12:23 -0500

    Listing 9.12 Shorten handler

--- bite/link/server.go
+++ bite/link/server.go
@@ -19,6 +19,35 @@ func NewServer(lg *slog.Logger) *Server {
 	return &Server{lg: lg}
 }
 
+// Shorten handles the URL shortening requests.
+//
+//	Status Code       Condition
+//	201               The link is successfully shortened.
+//	400               The request is invalid.
+//	409               The link already exists.
+//	405               The request method is not POST.
+//	413               The request body is too large.
+//	500               There is an internal error.
+func (srv *Server) Shorten(links *Store) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		lnk := Link{
+			Key: LinkKey(r.PostFormValue("key")),
+			URL: r.PostFormValue("url"),
+		}
+		if err := lnk.Validate(); err != nil {
+			httpError(w, fmt.Errorf("validating link: %w: %w",
+				err, bite.ErrInvalidRequest))
+			return
+		}
+		if err := links.Create(r.Context(), lnk); err != nil {
+			httpError(w, fmt.Errorf("creating link: %w", err))
+			return
+		}
+		w.WriteHeader(http.StatusCreated)
+		fmt.Fprint(w, lnk.Key)
+	})
+}
+
 // Health serves the health check requests.
 //
 //	Status Code       Condition
