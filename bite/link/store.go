3acabbaa635ca76255e13cae7d55a92ac7eddbb2
İnanç Gümüş <m@inanc.io>
2024-12-18 01:25:44 -0500

    Listing 9.4 Creating links

--- bite/link/store.go
+++ bite/link/store.go
@@ -1,6 +1,26 @@
 package link
 
+import (
+	"context"
+
+	"github.com/inancgumus/gobyexample/bite"
+)
+
 // Store persists and retrieves [Link] values in an in-memory map.
 type Store struct {
 	links map[LinkKey]Link
+}
+
+// Create persists a [Link] in the store.
+// It returns [bite.ErrInvalidRequest] if the [Link] is invalid
+// or [bite.ErrExists] if the [Link] already exists.
+func (s *Store) Create(_ context.Context, lnk Link) error {
+	if _, ok := s.links[lnk.Key]; ok {
+		return bite.ErrExists
+	}
+	if s.links == nil {
+		s.links = map[LinkKey]Link{}
+	}
+	s.links[lnk.Key] = lnk
+	return nil
 }
