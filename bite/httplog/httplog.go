709c4b0ee7f6ee2299d46cc2964bc39c012f4088
İnanç Gümüş <m@inanc.io>
2024-12-21 20:24:38 -0500

    Listing 10.2 Logging with attribute functions

--- bite/httplog/httplog.go
+++ bite/httplog/httplog.go
@@ -6,9 +6,13 @@ import (
 	"net/http"
 )
 
+// AttrFunc returns an [slog.Attr] to log.
+type AttrFunc func(*http.Request) slog.Attr
+
 // Logger logs incoming requests and responses.
 type Logger struct {
-	lg *slog.Logger
+	lg        *slog.Logger
+	attrFuncs []AttrFunc
 }
 
 // New returns a new [Logger].
@@ -16,12 +20,20 @@ func New(lg *slog.Logger) *Logger {
 	return &Logger{lg: lg}
 }
 
+// With augments logs with additional attributes.
+func (lg *Logger) With(funcs ...AttrFunc) *Logger {
+	lg.attrFuncs = append(lg.attrFuncs, funcs...)
+	return lg
+}
+
 // Wrap is a middleware that logs requests and responses.
 func (lg *Logger) Wrap(next http.Handler) http.Handler {
+	attrs := make([]slog.Attr, len(lg.attrFuncs))
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		next.ServeHTTP(w, r)
-		lg.lg.LogAttrs(r.Context(), slog.LevelInfo, "httplog",
-			slog.String("url", r.URL.String()),
-		)
+		for i, f := range lg.attrFuncs {
+			attrs[i] = f(r)
+		}
+		lg.lg.LogAttrs(r.Context(), slog.LevelInfo, "httplog", attrs...)
 	})
 }
